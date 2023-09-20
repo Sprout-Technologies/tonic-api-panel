627b1995cf13dd9dcf4eb95126b21d6ae74c4734
CoderMageFox <Codermagefox@codermagefox.com>
2023-09-20 13:13:56 +0800

    feat:滤镜功能 删除/编辑 前后端

--- src/views/works/form/MySelect.vue
+++ src/views/works/form/MySelect.vue
@@ -1,25 +1,38 @@
 <template>
-  <el-select v-model="selectedKey" :placeholder="placeholder">
-    <el-option v-for="(value, key) in options" :key="key" :label="key" :value="key">
-      {{key}}
+  <el-select v-model="model" placeholder="请选择">
+    <el-option
+      v-for="(optionValue, optionKey) in options"
+      :key="optionKey"
+      :label="optionKey"
+      :value="optionKey">
     </el-option>
   </el-select>
 </template>
 
 <script>
 export default {
-  props: ['value', 'options', 'placeholder'],
+  props: {
+    value: {
+      required: true
+    },
+    options: {
+      type: Object,
+      required: true
+    }
+  },
   computed: {
-    selectedKey: {
+    model: {
       get() {
-        return Object.keys(this.options).find(key => {
-          return this.options[key] === this.value;
-        });
+        return Object.keys(this.options).find(key => this.options[key] === this.value)
       },
-      set(key) {
-        this.$emit('input', this.options[key]);
+      set(newKey) {
+        this.$emit('input', this.options[newKey])
       }
     }
+  },
+  created() {
+    console.log('initial input value:', this.value)
+    console.log('initial options:', this.options)
   }
 }
 </script>
